6b104558b98d41de1648ee4bf9c3e54338474f0e
fisher <1186907891@qq.com>
2024-12-31 00:49:26 +0800

    feature: add a demo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("hello {}!", &name)
+}
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    println!("Start a server backend, listen:localhost:8000");
+    HttpServer::new(|| {
+        App::new()
+            // use get protocol
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    // support string name
+    .bind("localhost:8000")?
+    .run()
+    .await
 }
