ee9781740348337a6abe4c0ccc1f807d87ea5f76
fisher <1186907891@qq.com>
2024-12-31 16:42:54 +0800

    update: add a check func

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("world");
-    format!("hello {}!", &name)
+    format!("hello {}!\n", &name)
 }
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     println!("Start a server backend, listen:localhost:8000");
@@ -11,7 +16,7 @@ async fn main() -> Result<(), std::io::Error> {
         App::new()
             // use get protocol
             .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/{name}", web::get().to(health_check))
     })
     // support string name
     .bind("localhost:8000")?
