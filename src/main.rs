43af41964cfdd16cdd43c236eaf4f3f6c2776522
fisher <1186907891@qq.com>
2024-12-31 17:25:11 +0800

    feature: add a test framework

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("world");
-    format!("hello {}!\n", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    println!("Start a server backend, listen:localhost:8000");
-    HttpServer::new(|| {
-        App::new()
-            // use get protocol
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(health_check))
-    })
-    // support string name
-    .bind("localhost:8000")?
-    .run()
-    .await
+    run()?.await
 }
